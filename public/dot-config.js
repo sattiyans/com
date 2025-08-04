9eb998d196d24037e05c2e3901ba9afbaeb7bf19
Sattiyan Selvarajah <sattiyan@outlook.com>
2025-08-04 20:31:21 +0800

    fix

--- public/dot-config.js
+++ public/dot-config.js
@@ -3,5 +3,5 @@ window.DOT_CHATBOT = {
   dotId: 'f0208fe6-e88d-4e56-8455-03ccf99b0026',
   theme: 'dark',
   position: 'bottom-center',
-  welcomeMessage: 'Hi! I\'m your Dot assistant. Ask me anything about our company, products, or services.'
+  welcomeMessage: 'Hi! I\'m your Dot assistant. Ask me anything about my projects and services.'
 }; 
