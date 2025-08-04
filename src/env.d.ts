f60a5231d90216722cc9b9ab3f5072abb3836572
Sattiyan Selvarajah <sattiyan@outlook.com>
2025-08-04 20:22:26 +0800

    fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare global {
+  interface Window {
+    DOT_CHATBOT: {
+      dotId: string;
+      theme: string;
+      position: string;
+      welcomeMessage: string;
+    };
+  }
+}
+
+export {};
